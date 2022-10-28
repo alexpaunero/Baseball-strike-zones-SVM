30794408216438c084683021b0388a3eb7414506
alexpaunero <103605819+alexpaunero@users.noreply.github.com>
2022-10-28 23:15:22 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,27 @@
+import codecademylib3_seaborn
+import matplotlib.pyplot as plt
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from svm_visualization import draw_boundary
+from players import aaron_judge, jose_altuve, david_ortiz
+
+fig, ax = plt.subplots()
+
+# Task 1
+#print(aaron_judge.columns)
+
+# Task 2
+print(aaron_judge.description.unique())
+
+# Task 3
+print(aaron_judge.type.unique())
+
+# Task 4
+aaron_judge['type'] = aaron_judge['type'].map({'S': 1, 'B': 2})
+
+# Task 5
+print(aaron_judge['type'])
+
+# Task 6
+print(aaron_judge['plate_x'])
 
