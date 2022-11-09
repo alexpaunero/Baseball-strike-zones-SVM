99e16a9fd82df999f9f97d45941bee742c46b824
alexpaunero <103605819+alexpaunero@users.noreply.github.com>
2022-11-09 18:37:32 +0000

    Update script.py

--- script.py
+++ script.py
@@ -46,3 +46,19 @@ draw_boundary(ax, classifier)
 
 # Task 13
 print(classifier.score(validation_set[['plate_x', 'plate_z']], validation_set.type))
+
+# Task 15
+top_score = 0
+for gamma in range(1, 20):
+  for c in range(1, 20):
+    classifier = SVC(kernel='rbf', gamma = gamma, C = c)
+    classifier.fit(training_set[['plate_x','plate_z']], training_set.type)
+    score = classifier.score(validation_set[['plate_x', 'plate_z']], validation_set.type)
+    if score > top_score:
+      top_score = score
+      top_gamma = gamma
+      top_c = c
+
+print(top_score) 
+print(top_gamma) 
+print(top_c) 
